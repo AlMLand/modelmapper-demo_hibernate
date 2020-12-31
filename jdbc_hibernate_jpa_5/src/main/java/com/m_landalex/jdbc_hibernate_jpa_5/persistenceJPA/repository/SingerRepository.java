179bcd4b9727932ce0213bc82c03d1fd6592404e
AlMLand <morlandalex@googlemail.com>
2020-12-31 16:03:23 +0100

    Query fix.

--- jdbc_hibernate_jpa_5/src/main/java/com/m_landalex/jdbc_hibernate_jpa_5/persistenceJPA/repository/SingerRepository.java
+++ jdbc_hibernate_jpa_5/src/main/java/com/m_landalex/jdbc_hibernate_jpa_5/persistenceJPA/repository/SingerRepository.java
@@ -17,11 +17,11 @@ public interface SingerRepository extends JpaRepository<SingerEntity, Long> {
 	@Query("SELECT s FROM SingerEntity s WHERE s.birthDate=:birthDate")
 	List<SingerEntity> findByBirthDate(@Param("birthDate")LocalDate date);
 	
-	@Query("SELECT new com.morlandalex.jdbc_hibernate_jpa_5.data.SingerSummary(s.firstName, s.lastName, a.title) FROM SingerEntity s "
+	@Query("SELECT new com.m_landalex.jdbc_hibernate_jpa_5.data.SingerSummary(s.firstName, s.lastName, a.title) FROM SingerEntity s "
 			+ "LEFT JOIN s.albums a WHERE a.releaseDate=(SELECT max(a2.releaseDate) FROM AlbumEntity a2 WHERE a2.singerId.id=s.id) ORDER BY s.id")
 	List<SingerSummary> findAllSingerSummary();
 	
-	@Query("SELECT new com.morlandalex.jdbc_hibernate_jpa_5.data.SingerSummary(s.firstName, s.lastName, a.title) FROM SingerEntity s "
+	@Query("SELECT new com.m_landalex.jdbc_hibernate_jpa_5.data.SingerSummary(s.firstName, s.lastName, a.title) FROM SingerEntity s "
 			+ "LEFT JOIN s.albums a WHERE a.releaseDate=(SELECT max(a2.releaseDate) FROM AlbumEntity a2 WHERE a2.singerId.id=s.id) AND s.id=:id")
 	SingerSummary findByIdSingerSummary(@Param("id")Long singerId);
 	
